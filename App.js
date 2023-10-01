81123856228fcf911d0af55790a08c75f93a59c7
18Codemitra <103235445+18Codemitra@users.noreply.github.com>
2023-10-01 20:31:12 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+
+import{useState} from 'react'
+import Home from './Screens./Home' 
+import AllRouting from './Routing/AllRouting';
+
+
+
+function App(){
+
+
+ 
+
+ 
+
+
+
+
+
+  return (
+    
+<AllRouting/>
+   )
+
+    }
+  
+
+export default App;
