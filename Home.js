1ff15452788cdf3d579b3c07bde0990cef77e421
18Codemitra <103235445+18Codemitra@users.noreply.github.com>
2023-10-01 20:35:28 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,13 @@
+
+
+
+
+function Home({color}){
+    return( 
+         <>
+        <h1> This is home component. </h1>
+        <div style ={{width :100 , height :100 , backgroundColor: color }} ></div>
+        </>
+    )
+}
+export default Home; 
